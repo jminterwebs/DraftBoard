f1c77ad645dc9ab94bd3711590a7d62280518fa4
John <jmverdone@gmail.com>
2015-08-26 00:55:58 -0400

    basic filter

---
+++ Public/javascript/filters/playerSearch.js
@@ -0,0 +1,18 @@
+angular.module('DraftBoard').filter('playerSearch', function(){
+    return function(input, position){
+
+
+
+       if({position: "All"} ){
+        return console.log(Boolean({position: "All"}));
+
+       }
+
+
+
+
+
+
+    };
+
+});
