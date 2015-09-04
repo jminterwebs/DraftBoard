a117d221e0c18af32da15ca2a1585c87a922513a
John <jmverdone@gmail.com>
2015-09-03 20:37:00 -0400

    linting changes.

--- Public/javascript/controller/DraftlistController.js
+++ Public/javascript/controller/DraftlistController.js
@@ -1,13 +1,10 @@
-angular.module('DraftBoard').controller('DraftlistController', function(Draft, $scope, $http, $routeParams, $location){
-
+angular.module('DraftBoard').controller('DraftlistController', function(Draft, $scope){
 
     $scope.playerlist = Draft.query();
 
     $scope.PlayerSelected = function($event, player){
 
-
-    $scope.player = player.list;
-
+        $scope.player = player.list;
     };
 
     $scope.nameSearch = {
@@ -15,48 +12,39 @@ angular.module('DraftBoard').controller('DraftlistController', function(Draft, $
         position: 'All'
     };
 
-      var draftpick = 1;
-      var draftTeam = 1;
-      var draftround = 0;
-      var draftTeamUp = 0;
-      var draftTeam = 11;
-    $scope.selectPlayer = function(player){
+      var draftpick = 1,
+          draftround = 0,
+          draftTeamUp = 0,
+          draftTeam = 11;
 
+    $scope.selectPlayer = function(){
 
-        //  Draft round
+     if($scope.player){
+
+         //  Draft round
         if(draftpick % 10 == 1) {
          draftround ++;
         }
 
-        // Sets odd rounds to pick up even rounds to pick down.
-        if(draftround % 2 == !0){
-                draftTeam = 11;
-               draftTeamUp ++;
+        // Sets odd rounds to pick up even rounds to pick down.dsdsd
+        if(draftround % 2){
+            draftTeam = 11;
+            draftTeamUp ++;
             $scope.player.DraftedTeamPosition = draftTeamUp;
         }else {
-            draftTeamUp = 0
+            draftTeamUp = 0;
             draftTeam--;
             $scope.player.DraftedTeamPosition = draftTeam;
         }
 
-      $scope.player.DraftedRound = draftround;
-      $scope.player.DraftedPick  = draftpick;
-      $scope.player.DraftedTeamName = "BenchWarmers";
+            $scope.player.DraftedRound = draftround;
+            $scope.player.DraftedPick  = draftpick;
+            $scope.player.DraftedTeamName = "BenchWarmers";
 
-
-      var value = $scope.playerlist.indexOf($scope.player);
-
-      //remove from draft list
-      //$scope.playerlist.splice(value, 1);
-
-
-        //increment the pick
-        draftpick ++;
-
-
-
-     console.log("Draft pick " + $scope.player.DraftedPick + " Drafted Round " + $scope.player.DraftedRound + " Team Drafted " + $scope.player.DraftedTeamPosition );
-        $scope.player = "";
+        //increment the pick and clear the selection
+            draftpick ++;
+            $scope.player = "";
+     }
 
     };
 
